77c1a3b6c0eb9c0625a9587d13719b5fd84ec225
Dileep Vijayakumar <dileepvk98@gmail.com>
2023-01-27 12:37:04 +0530

    Add files via upload
    updated for multiple notation in 1 img

--- coco_to_yolo.py
+++ coco_to_yolo.py
@@ -2,26 +2,31 @@ import json
  
 with open('toy_human_coco.json', 'r') as coco_f:
     data = json.load(coco_f)
- 
 print(len(data['images']), len(data['annotations']))
- 
-# yolo_f = open('toy_human_yolo.txt', 'w')
- 
-for i in range(len(data['images'])):
-    path = data['images'][i]['path']
-    category_id = data['images'][i]['category_ids'][0]
+
+img_id_path_map = {i["id"] : i["path"] for i in data["images"]}
+# print(img_id_path_map)
+
+min_cat_id = min([c["id"] for c in data["categories"]])
+print('min_cat_id :',min_cat_id)
+
+cat_id_map = {c['name'] : c['id'] for c in data["categories"]}
+print(cat_id_map)
+# categories = list(cat_id_map.keys())
+# print(categories)
+
+for a in data['annotations']:
+    path = img_id_path_map[a["image_id"]]
+    category_id = a["category_id"]-min_cat_id
     
     # coco format of bbox - xmin, ymin, w, h
-    bbox = data['annotations'][i]['bbox']
-    w, h = data['annotations'][i]['width'], data['annotations'][i]['height']
+    bbox = a['bbox']
+    w, h = a['width'], a['height']
     
-    # print(path.split('/')[-1].replace(".jpg",""))
-    
-    with open('yolo_data/'+path.split('/')[-1].replace(".jpg","")+".txt", 'w') as f:
+    # print(path.split('/')[-1].replace(".jpg",""))    
+    with open('yolo_data/'+path.split('/')[-1].replace(".jpg","")+".txt", 'a') as f:
         # yolov4 format of bbox = xcenter, ycenter, w, h
         xc = (bbox[0]+bbox[2]/2)/w
         yc = (bbox[1]+bbox[3]/2)/h
-        f.write(str(0)+" "+str(xc)+" "+str(yc)+" "+str(bbox[2]/w)+" "+str(bbox[3]/h)+"\n")
-    
-    # yolo_f.write(str(0)+" "+str(bbox[0]/w)+" "+str(bbox[1]/h)+" "+str(bbox[2]/w)+" "+str(bbox[3]/h)+"\n")
+        f.write(str(category_id)+" "+str(xc)+" "+str(yc)+" "+str(bbox[2]/w)+" "+str(bbox[3]/h)+"\n")
  
